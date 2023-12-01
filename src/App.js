c011b60815792f9425fe74a703a66ea8942407a7
TusharRB <tusharbotare17@gmail.com>
2023-12-01 17:17:54 +0530

    Context Api Done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Routes>
 
-          <Route exact path='/' element={<Home/>}  />
-          <Route exact path='/about' element={<About />}  />
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/about' element={<About />} />
 
-        </Routes>
-      </Router>
+          </Routes>
+        </Router>
+      </NoteState>
+
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,17 @@
-import React from 'react' //rafce
+import React, { useContext, useEffect } from 'react'
+import noteContext from '../context/notes/noteContext.js'
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
+    const a = useContext(noteContext)
+    useEffect(() => {
+        a.update();
+        // eslint-disable-next-line
+    }, [])
+    return (
+        <div>
+            This is About {a.state.name} and he is in class {a.state.class}
+        </div>
+    )
 }
 
 export default About
