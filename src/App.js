53a90b5ccddc2f7ce28d8c722711bb05c4371b29
TusharRB <tusharbotare17@gmail.com>
2023-12-01 16:33:34 +0530

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNoteBook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+
+          <Route exact path='/' element={<Home/>}  />
+          <Route exact path='/about' element={<About />}  />
+
+        </Routes>
+      </Router>
+    </>
   );
 }
 
